9637b9a0f2d403aae101239c6caa7d8e06df8904
Anmy Moreno <nayle2701@gmail.com>
2022-07-26 18:04:05 -0400

    start-10

--- src/assets/components/Card_Pass.jsx
+++ src/assets/components/Card_Pass.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Card_Pass = () => {
-  return (
-    <div className='card'>
-        <div className="passw">
-            <label htmlFor="Contraseña"></label>
-            <input type="text" />
-        </div>
 
+  const [valueText, setvalueText] = useState('password')
+
+  const toggleStateValue= () => {
+    if (valueText==='password'){
+        setvalueText('text')
+    }else{
+        setvalueText('password')
+    }
+
+  }
+
+  return (
+    <div className='card_passw'>
+        <label htmlFor="pass">Contraseña</label>
+        <div className="passw">
+            <input className='passw_input' type={valueText}/>
+            <button onClick={toggleStateValue} className='btn-options2'> <i className='bx bxs-low-vision'></i> </button>
+        </div>
     </div>
   )
 }
